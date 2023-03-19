58999010d9d799f1d6a8e9cf310390db72d6795d
DatNguyen <vinhdat4999@gmail.com>
2023-03-19 18:02:13 +0700

    #comment: release v2.0.0

--- src/main/java/com/blas/blasemail/configuration/WebSecurityConfig.java
+++ src/main/java/com/blas/blasemail/configuration/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig {
       throws Exception {
     http.csrf().disable();
     http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    http.authorizeRequests().anyRequest().permitAll();
+    http.authorizeRequests().anyRequest().authenticated();
     return http.build();
   }
 
--- src/main/java/com/blas/blasemail/email/HtmlEmail.java
+++ src/main/java/com/blas/blasemail/email/HtmlEmail.java
@@ -1,13 +1,13 @@
 package com.blas.blasemail.email;
 
+import static com.blas.blascommon.utils.TemplateUtils.generateHtmlContent;
+
 import com.blas.blascommon.payload.HtmlEmailRequest;
-import com.blas.blascommon.payload.HtmlEmailResponse;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.CountDownLatch;
 import javax.mail.MessagingException;
-import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -16,30 +16,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class HtmlEmail extends Email {
 
-  public HtmlEmailResponse sendEmail(List<HtmlEmailRequest> htmlEmailPayloadList)
-      throws MessagingException {
-    MimeMessage mail = javaMailSender.createMimeMessage();
-    MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-    helper.setFrom(new InternetAddress(mailProperties.getUsername()));
-    AtomicInteger sentEmailNum = new AtomicInteger();
-    List<HtmlEmailRequest> htmlEmailRequestFailedList = new ArrayList<>();
-    htmlEmailPayloadList.forEach(htmlEmailPayload -> {
+  public void sendEmail(HtmlEmailRequest htmlEmailRequest, List<HtmlEmailRequest> sentEmailList,
+      List<HtmlEmailRequest> failedEmailList, CountDownLatch latch) {
+    new Thread(() -> {
+      MimeMessage message = javaMailSender.createMimeMessage();
+      MimeMessageHelper helper = new MimeMessageHelper(message);
       try {
-        String htmlContent = generateHtmlContent(htmlEmailPayload.getEmailTemplateName(),
-            htmlEmailPayload.getData());
-        helper.setTo(htmlEmailPayload.getEmailTo());
-        helper.setSubject(htmlEmailPayload.getTitle());
+        helper.setTo(htmlEmailRequest.getEmailTo());
+        helper.setSubject(htmlEmailRequest.getTitle());
+        String htmlContent = generateHtmlContent(templateEngine,
+            htmlEmailRequest.getEmailTemplateName(), htmlEmailRequest.getData());
         helper.setText(htmlContent, true);
-        javaMailSender.send(mail);
-        sentEmailNum.getAndIncrement();
-      } catch (MessagingException e) {
-        saveCentralizeLog(e, htmlEmailPayload);
-        htmlEmailRequestFailedList.add(htmlEmailPayload);
+        javaMailSender.send(message);
+        sentEmailList.add(htmlEmailRequest);
+      } catch (MailException | MessagingException e) {
+        saveCentralizeLog(e, htmlEmailRequest);
+        failedEmailList.add(htmlEmailRequest);
+      } finally {
+        latch.countDown();
       }
-    });
-    HtmlEmailResponse htmlEmailResponse = new HtmlEmailResponse();
-    htmlEmailResponse.setSentEmailNum(sentEmailNum.get());
-    htmlEmailResponse.setHtmlEmailRequestFailedList(htmlEmailRequestFailedList);
-    return htmlEmailResponse;
+    }).start();
   }
 }
