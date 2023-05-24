351101b45e2a1e95831f9605085155152207809d
DatNguyen <vinhdat4999@gmail.com>
2023-05-25 02:31:55 +0700

    #comment: release v2.7.0

--- src/main/java/com/blas/blasemail/configuration/WebSecurityConfig.java
+++ src/main/java/com/blas/blasemail/configuration/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -40,10 +42,13 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http, JwtRequestFilter jwtRequestFilter)
       throws Exception {
-    http.csrf().disable();
-    http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    http.authorizeHttpRequests().anyRequest().authenticated();
-    return http.build();
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+        .authorizeHttpRequests(authorize -> authorize
+            .anyRequest().authenticated())
+        .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
+        .build();
   }
 
   @Bean
